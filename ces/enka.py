bee13ac096ed266b23d100684c07c94c5be4b502
Alfredo Garbuno <agarbuno@caltech.edu>
2019-07-11 10:21:58 +0200

    added some documentation for the eka algorithms

--- ces/enka.py
+++ ces/enka.py
@@ -11,7 +11,7 @@ import multiprocessing
 class eki(object):
 
 	def __init__(self, p, n_obs, J, l_window = 0, freq = 0):
-		self.n_obs = n_obs		    # Dimensionality of statistics (observations)
+		self.n_obs = n_obs		  # Dimensionality of statistics (observations)
 		self.p = p                # Dimensionality of theta (parameters)
 		self.J = J                # Number of ensemble particles
 		self.epsilon = 1e-7       # Underflow protection
@@ -101,14 +101,26 @@ class eki(object):
 	    """
 		pass
 
-	def G(self, k, model):
+	def G(self, theta, model):
 		"""
-		General evaluation function without partition of the model parameters
+		General evaluation function without partition of the model parameters.
+		To be used as a black box. It evaluates single case.
+		Inputs:
+			- theta: [p, ] dimensional array
+			- model: forward map name
 		"""
-		g = model(k)
+		g = model(theta)
 		return g
 
 	def Gpar(self, theta, model):
+		"""
+		Evaluates for a collection of particles. If parallel is set to true, then
+		it uses the available cores as initialized with the eki object.
+		Inputs:
+			- theta: [p, N] array where N is the number of particles to be evaluated
+				and p is the dimensionality of the parameters.
+			- model: model name for the forward model.
+		"""
 		if self.parallel:
 			Geval = Parallel(n_jobs=self.num_cores)(delayed(self.G)(k, model) for k in theta.T)
 			return (np.asarray(Geval).T)
@@ -119,6 +131,20 @@ class eki(object):
 			return Gs
 
 	def G_pde(self, k, model, t):
+		"""
+		Forward model for PDE constrained inverse problem. If parallel is set to
+		true, then it uses the available cores as initialized with the eki object.
+
+		(WARNING): Only used for Lorenz63
+
+		Inputs:
+			- k: [p + n, ], p is the dimensionality of the parameters, n is the
+				dimensionality of the state used as initial conditions for
+				the numerical integrator.
+			- t: [t_eks,], array with computational times to evaluate the numerical
+				integrator.
+			- model: model name for the forward model.
+		"""
 		r, b = k[:self.p]
 		w0 = k[self.p:]
 
@@ -127,18 +153,17 @@ class eki(object):
 		ws = [xs, ys, zs, xs**2, ys**2, zs**2, xs*ys, xs*zs, ys*zs]
 
 		# With rolling statistics
-		gs = [np.asarray(pd.Series(k).rolling(window = self.l_window * self.freq ).mean()) for k in ws]
+		gs = [np.asarray(pd.Series(k).rolling(window = int(self.l_window * self.freq) ).mean()) for k in ws]
 		gs = np.asarray(gs)[:,-1]
 
 		# With adjacent windows
 		# gs = np.array([k[1:].reshape(-1, self.l_window * self.freq).mean(axis = 1) for k in ws])
 		# gs = gs[:,-1]
 
-
 		return np.concatenate([gs, np.asarray([xs[-1], ys[-1], zs[-1]])])
 
 	def Gpar_pde(self, theta, model, t):
-	    Geval = Parallel(n_jobs=self.num_cores)(delayed(self.G)(k, model, t) for k in theta.T)
+	    Geval = Parallel(n_jobs=self.num_cores)(delayed(self.G_pde)(k, model, t) for k in theta.T)
 	    return (np.asarray(Geval).T)
 
 	def scale_ensemble(self, factor = 2.):
@@ -244,77 +269,6 @@ class flow(eki):
 		self.Ustar = self.Uall[-1]
 		self.Gstar = self.Gpar(self.Ustar, model)
 
-	def run_nopar(self, y_obs, U0, model, Gamma, Jnoise, verbose = True):
-		"""
-		Find the minimizer of an inverse problem using the continuous time limit
-		of the EnKF
-
-		Inputs:
-		- U0: A numpy array of shape (p, J) initial ensemble; there are J
-		  	ensemble particles each of dimension p.
-		- y_obs: A numpy array of shape (n_obs,) of observed data.
-		- wt: A numpy array of initial conditions to start the ensemble when
-			evaluating the forward model
-		- t: A numpy array of time points where the ODE is evaluated
-		- ...
-		- verbose: (boolean) If true, print progress during iterations.
-
-		Outputs:
-		- None
-		"""
-		# Storing the ensemble members
-		self.Uall = []; self.Uall.append(U0)
-		self.radspec = []
-
-		# Storing metrics
-		self.metrics = dict()
-		self.metrics['v'] = [] 			# Tracks collapse in parameter space
-		self.metrics['V'] = []			# Tracks collapse after forward model evaln
-		self.metrics['R'] = []			# Tracks data-fitting
-		self.metrics['r'] = [] 			# Tracks the collapse towards the truth
-		self.t = []
-
-		for i in tqdm(range(self.T), desc = 'Ens.size: %s'%(self.J)):
-		    Geval = self.Gpar(U0, model)
-
-		    # For ensemble update
-		    E = Geval - Geval.mean(axis = 1)[:,np.newaxis]
-		    R = Geval - y_obs[:,np.newaxis]
-		    D =  (1.0/self.J) * np.matmul(E.T, np.linalg.solve(Gamma, R))
-
-		    # Track metrics
-		    self.metrics['v'].append(((U0 - U0.mean(axis = 1)[:, np.newaxis])**2).sum(axis = 0).mean())
-		    self.metrics['r'].append(((U0 - self.ustar)**2).sum(axis = 0).mean())
-		    self.metrics['V'].append((np.diag(np.matmul(E.T, np.linalg.solve(Gamma, E)))**2).mean())
-		    self.metrics['R'].append((np.diag(np.matmul(R.T, np.linalg.solve(Gamma, R)))**2).mean())
-
-		    # Compute the update
-		    self.radspec.append(np.linalg.eigvals(D).real.max())
-		    #hk = 1./(np.linalg.norm(D) + 1e-8)
-		    hk = 1./self.radspec[-1]
-		    #hk = 0.01
-
-		    if i == 0:
-		    	self.t.append(hk)
-		    else:
-		    	self.t.append(hk + self.t[-1])
-		    Umean = U0.mean(axis = 1)[:, np.newaxis]
-		    Ucov  = np.cov(U0) + 1e-8 * np.identity(self.p)
-
-		    Ustar = np.linalg.solve(np.eye(self.p) + hk/(self.sigma**2) * Ucov,
-		    	U0 - hk * np.matmul(U0 - Umean, D) + hk/(self.sigma**2) * np.matmul(Ucov, self.mu))
-		    Uk = Ustar + np.sqrt(2*hk) * np.matmul( np.linalg.cholesky(Ucov), np.random.normal(0, 1, [self.p, self.J]))
-
-		    self.Uall.append(Uk)
-		    U0 = Uk
-
-		    if self.t[-1] > 2:
-		    	break
-
-		self.Uall = np.asarray(self.Uall)
-		self.Ustar = self.Uall[-1]
-		self.Gstar = self.Gpar(self.Ustar, model)
-
 	def run_sde(self, y_obs, U0, model, Gamma, Jnoise, verbose = True):
 		"""
 	    Find the minimizer of an inverse problem using the continuous time limit
@@ -388,81 +342,6 @@ class flow(eki):
 
 		self.Uall = np.asarray(self.Uall)
 
-	def run_sdenopar(self, y_obs, U0, model, Gamma, Jnoise, verbose = True):
-		"""
-	    Find the minimizer of an inverse problem using the continuous time limit
-	    of the EnKF
-
-	    Inputs:
-	    - U0: A numpy array of shape (p, J) initial ensemble; there are J
-	      	ensemble particles each of dimension p.
-	    - y_obs: A numpy array of shape (n_obs,) of observed data.
-	    - wt: A numpy array of initial conditions to start the ensemble when
-	    	evaluating the forward model
-	    - t: A numpy array of time points where the ODE is evaluated
-	    - ...
-	    - verbose: (boolean) If true, print progress during iterations.
-
-	    Outputs:
-	    - None
-	    """
-
-		# Storing the ensemble members
-		self.Uall = []; self.Uall.append(U0)
-		self.radspec = []
-
-		# Storing metrics
-		self.metrics = dict()
-		self.metrics['v'] = [] 			# Tracks collapse in parameter space
-		self.metrics['V'] = []			# Tracks collapse after forward model evaln
-		self.metrics['R'] = []			# Tracks data-fitting
-		self.metrics['r'] = [] 			# Tracks the collapse towards the truth
-		self.t = []                 # Tracks simulated time
-
-		for i in tqdm(range(self.T)):
-		    Geval = self.Gnopar(U0, model)
-
-		    # For ensemble update
-		    E = Geval - Geval.mean(axis = 1)[:,np.newaxis]
-		    R = Geval - y_obs[:,np.newaxis]
-		    D =  (1.0/self.J) * np.matmul(E.T, np.linalg.solve(Gamma, R))
-		    S = -(1.0/self.J) * np.matmul(E.T, np.linalg.solve(Gamma, \
-		    		np.matmul(Jnoise, (np.random.normal(0,1,[self.n_obs, self.J])))
-		    		)
-		    	)
-
-		    # For tracking metrics
-		    self.metrics['v'].append(((U0 - U0.mean(axis = 1)[:, np.newaxis])**2).sum(axis = 0).mean())
-		    self.metrics['r'].append(((U0 - self.ustar)**2).sum(axis = 0).mean())
-		    self.metrics['V'].append((np.diag(np.matmul(E.T, np.linalg.solve(Gamma, E)))**2).mean())
-		    self.metrics['R'].append((np.diag(np.matmul(R.T, np.linalg.solve(Gamma, R)))**2).mean())
-
-		    self.radspec.append(np.linalg.eigvals(D).real.max())
-		    # hk = 1./(np.linalg.norm(D + S) + 1e-8)
-		    hk = 1./self.radspec[-1]
-		    if i == 0:
-		    	self.t.append(hk)
-		    else:
-		    	self.t.append(hk + self.t[-1])
-		    # Explicit
-		    # Uk = U0 - hk * np.matmul(U0, D)
-		    Umean = U0.mean(axis = 1)[:, np.newaxis]
-		    # Uk = U0 - hk * np.matmul(U0 - Umean, D + S)
-		    Uk = U0 - hk * np.matmul(U0 - Umean, D) - np.sqrt(2 * hk) * np.matmul(U0 - Umean, S)
-		    # Uk[Uk<0.] = 0.
-		    # Implicit
-		    # Uk = np.linalg.solve(np.eye(J) + hk * D.T, U0.T).T
-		    # Uk = np.linalg.solve(np.eye(J) + hk * D.T, (U0 - U0.mean(axis = 1)[:, np.newaxis]).T).T + U0.mean(axis = 1)[:, np.newaxis]
-		    # Uk = np.linalg.solve(np.eye(J) + (hk/2) * D.T, np.matmul(np.eye(J) - (hk/2) * D.T, U0.T)).T
-		    # Uk = U0 - (hk / (1 + hk * np.diag(D))) * np.matmul(U0, D)
-
-		    self.Uall.append(Uk)
-		    U0 = Uk
-
-		self.Uall = np.asarray(self.Uall)
-		self.Ustar = self.Uall[-1]
-		self.Gstar = self.Gnopar(self.Ustar, model)
-
 	def run_pde(self, y_obs, U0, wt, t, model, Gamma, Jnoise, verbose = True):
 		"""
 	    Find the minimizer of an inverse problem using the continuous time limit
@@ -496,7 +375,7 @@ class flow(eki):
 		self.t = []
 
 		for i in tqdm(range(self.T)):
-		    Geval = self.Gpar(np.vstack([U0, self.W0]), model, t)
+		    Geval = self.Gpar_pde(np.vstack([U0, self.W0]), model, t)
 		    self.W0 = Geval[self.n_obs:,:]
 		    Geval = Geval[:self.n_obs,:]
 
@@ -504,12 +383,8 @@ class flow(eki):
 		    E = Geval - Geval.mean(axis = 1)[:,np.newaxis]
 		    R = Geval - y_obs[:,np.newaxis]
 		    D =  (1.0/self.J) * np.matmul(E.T, np.linalg.solve(Gamma, R))
-		    S = -(1.0/self.J) * np.matmul(E.T, np.linalg.solve(Gamma, \
-		    		np.matmul(Jnoise, (np.random.normal(0,1,[self.n_obs, self.J])))
-		    		)
-		    	)
 
-		     # For tracking metrics
+		    # Track metrics
 		    self.metrics['v'].append(((U0 - U0.mean(axis = 1)[:, np.newaxis])**2).sum(axis = 0).mean())
 		    self.metrics['r'].append(((U0 - self.ustar)**2).sum(axis = 0).mean())
 		    self.metrics['V'].append((np.diag(np.matmul(E.T, np.linalg.solve(Gamma, E)))**2).mean())
@@ -521,22 +396,28 @@ class flow(eki):
 		    	self.t.append(hk)
 		    else:
 		    	self.t.append(hk + self.t[-1])
-		    # Explicit
-		    # Uk = U0 - hk * np.matmul(U0, D)
 		    Umean = U0.mean(axis = 1)[:, np.newaxis]
+		    Ucov  = np.cov(U0) + 1e-8 * np.identity(self.p)
+
 		    #Uk = np.abs(U0 - hk * np.matmul(U0 - Umean, D + S))
-		    Uk = np.abs(U0 - hk * np.matmul(U0 - Umean, D) - np.sqrt(2*hk) * np.matmul(U0 - Umean, S))
-		    # Uk[Uk<0.] = 0.
-		    # Implicit
-		    # Uk = np.linalg.solve(np.eye(J) + hk * D.T, U0.T).T
-		    # Uk = np.linalg.solve(np.eye(J) + hk * D.T, (U0 - U0.mean(axis = 1)[:, np.newaxis]).T).T + U0.mean(axis = 1)[:, np.newaxis]
-		    # Uk = np.linalg.solve(np.eye(J) + (hk/2) * D.T, np.matmul(np.eye(J) - (hk/2) * D.T, U0.T)).T
-		    # Uk = U0 - (hk / (1 + hk * np.diag(D))) * np.matmul(U0, D)
+		    #Uk = np.abs(U0 - hk * np.matmul(U0 - Umean, D) - np.sqrt(2*hk) * np.matmul(U0 - Umean, S))
+		    Ustar = np.linalg.solve(np.eye(self.p) + hk/(self.sigma**2) * Ucov,
+		    	U0 - hk * np.matmul(U0 - Umean, D) + hk/(self.sigma**2) * np.matmul(Ucov, self.mu))
+		    Uk = np.abs(Ustar + np.sqrt(2*hk) * np.matmul( np.linalg.cholesky(Ucov),
+		    np.random.normal(0, 1, [self.p, self.J])))
 
 		    self.Uall.append(Uk)
 		    U0 = Uk
 
+		    if self.t[-1] > 2:
+		    	break
+
 		self.Uall = np.asarray(self.Uall)
+		self.Ustar = self.Uall[-1]
+		Geval = self.Gpar_pde(np.vstack([self.Ustar, self.W0]), model, t)
+		self.W0 = Geval[self.n_obs:,:]
+		self.Gstar = Geval = Geval[:self.n_obs,:]
+
 
 	def run_data(self, y_obs, data, U0, wt, t, model, Gamma, Jnoise, verbose = True):
 		"""
