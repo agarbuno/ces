6bf7683e00e6f722f3f8cb66c2c1d3f049b33a89
agarbuno <agarbuno@caltech.edu>
2019-08-01 16:16:30 -0700

    Created lorenz63 model

--- ces/utils.py
+++ ces/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import integrate
 
 def banana(u, a = 1., b = .5, flag_noise = False):
     rho = 0.95
@@ -23,6 +24,53 @@ def elliptic(u, x1 = 1./4, x2 = 3./4, dG = False, flag_noise = True, noise = 0.1
     	return DG
     else:
     	return [x,y]
+
+class lorenz63(object):
+    '''Lorenz '63 model'''
+    def __init__(self, l_window = 10, freq = 100):
+        """
+        Lorenz 63 model initilization
+        """
+        self.n_state = 3
+        self.n_obs = 9
+        self.l_window = l_window
+        self.freq = freq
+
+    def __call__(self, w, t, r = 28., b = 8./3):
+        """
+        Reduced Lorenz 63 model with 2 parameters
+        """
+        x_dot, y_dot, z_dot = self.model(w, t, 10., r, b)
+        return [x_dot, y_dot, z_dot]
+
+    def model(self, w, t, sigma = 10., r = 28., b = 8./3):
+        """
+        Original Lorenz 63 model with 3 parameters
+        """
+        x, y, z = w
+        x_dot = sigma * (y - x)
+        y_dot = r * x - y - x * z
+        z_dot = x * y - b * z
+        return [x_dot, y_dot, z_dot]
+
+    def solve(self, w0, t):
+        """
+        Solve the Lorenz system using the callable object defined in __call__.
+        Inputs:
+            - w0: [n_state, ] initial conditions
+            - t : time vector to collect solution of ODE solver
+        """
+        ws = integrate.odeint(self, w0, t)
+        return ws
+
+    def statistics(self, ws):
+        """
+        """
+        xs, ys, zs = ws[:,0], ws[:,1], ws[:,2]
+        ws = [xs, ys, zs, xs**2, ys**2, zs**2, xs*ys, xs*zs, ys*zs]
+        gs = np.asarray(ws)[:,1:].reshape(self.n_obs, -1,
+            int(self.l_window * self.freq)).mean(axis = 2)[:,-1]
+        return np.round(gs, 12)
 
 def lorenz(w, t0, sigma = 10., r = 28., b = 8./3):
 	"""
@@ -58,23 +106,6 @@ def lorenz96_rhs(X, t, h, F, c, b):
 
     return np.hstack((dXdt, dYdt))
 
-def lorenz96_ivp(t, X, h = 1., F = 10., c = 10., b = 10.):
-    n_slow = 36       # Slow variables
-    n_fast = 10       # Fast variables
-
-    Y = X[n_slow:]
-    X = X[:n_slow]
-    dXdt = np.zeros(X.shape)
-    dYdt = np.zeros(Y.shape)
-    for k in range(n_slow):
-        dXdt[k] = -X[k - 1] * (X[k - 2] - X[(k + 1) % n_slow]) - X[k] + F - \
-                    (h * c) * np.mean(Y[k * n_fast: (k + 1) * n_fast])
-    for j in range(n_fast * n_slow):
-        dYdt[j] = -c * b * Y[(j + 1) % (n_fast * n_slow)] * (Y[(j + 2) % (n_fast * n_slow)] - \
-                    Y[j-1]) - c * Y[j] + ((h*c)/n_fast) * X[int(j / n_fast)]
-
-    return np.hstack((dXdt, dYdt))
-
 def lorenz96_dim(t, X, h = 1., F = 10., c = 2**7., b = 1.):
     n_slow = 36       # Slow variables
     n_fast = 10       # Fast variables
@@ -92,6 +123,32 @@ def lorenz96_dim(t, X, h = 1., F = 10., c = 2**7., b = 1.):
                     c * Y[j] + c * X[int(j / n_fast)]
 
     return np.hstack((dXdt, dYdt))
+
+def lorenz96_ivp(t, X, h = 1., F = 10., c = 10., b = 10.):
+    n_slow = 36       # Slow variables
+    n_fast = 10       # Fast variables
+
+    Y = X[n_slow:]
+    X = X[:n_slow]
+    dXdt = np.zeros(X.shape)
+    dYdt = np.zeros(Y.shape)
+    for k in range(n_slow):
+        dXdt[k] = -X[k - 1] * (X[k - 2] - X[(k + 1) % n_slow]) - X[k] + F - \
+                    (h * c) * np.mean(Y[k * n_fast: (k + 1) * n_fast])
+    for j in range(n_fast * n_slow):
+        dYdt[j] = -c * b * Y[(j + 1) % (n_fast * n_slow)] * (Y[(j + 2) % (n_fast * n_slow)] - \
+                    Y[j-1]) - c * Y[j] + ((h*c)/n_fast) * X[int(j / n_fast)]
+
+    return np.hstack((dXdt, dYdt))
+
+def generate_statistics(data):
+    Phi = np.vstack([data[:n_slow].mean(axis = 1),            # <  X >
+                 (data[:n_slow]**2).mean(axis = 1),           # < X2 >
+                 data[n_slow:].reshape(n_slow, n_fast, -1).mean(axis = 1).mean(axis = 1),      # < Ybar  >
+                 (data[n_slow:]**2).reshape(n_slow, n_fast, -1).mean(axis = 1).mean(axis = 1), # < Y2bar >
+                 (data[:n_slow] * (data[n_slow:]).reshape(n_slow, n_fast, -1).mean(axis = 1)).mean(axis = 1)])  # < X Y2bar >
+
+    return Phi.reshape(1,-1)
 
 def scale_gppreds(gpmeans, gpvars, Gmean, Gstd):
 	"""
