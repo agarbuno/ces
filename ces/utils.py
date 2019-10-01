4e99bafb868c743ce9e54834044525b8b7435336
Alfredo Garbuno <agarbuno@caltech.edu>
2019-10-01 11:59:19 -0700

    added elliptic problem

--- ces/utils.py
+++ ces/utils.py
@@ -29,6 +29,39 @@ class lineal(object):
 		else:
 			return np.matmul(self.A, theta)
 
+class elliptic(object):
+	"""
+	"""
+	def __init__(self, flag_noise = False):
+		"""
+		"""
+		self.x1 = 1./4
+		self.x2 = 3./4
+		self.flag_noise = flag_noise
+		self.noise      = 0.1
+		self.model_name = 'elliptic'
+		self.type       = 'map'
+
+	def __repr__(self):
+		return self.model_name
+
+	def __str__(self):
+		return self.model_name
+
+	def __call__(self, theta, dG = False):
+		"""
+		"""
+		u1, u2 = theta
+		x = (u2 * self.x1) + (np.exp(-u1) * (-self.x1**2+self.x1) * 0.5) + (self.flag_noise * self.noise * np.random.normal())
+		y = (u2 * self.x2) + (np.exp(-u1) * (-self.x2**2+self.x2) * 0.5) + (self.flag_noise * self.noise * np.random.normal())
+
+		if dG:
+			DG = np.array([[-np.exp(-u1) * (-self.x1**2+self.x1) * 0.5, self.x1],
+						   [-np.exp(-u1) * (-self.x2**2+self.x2) * 0.5, self.x2]])
+			return DG
+		else:
+			return [x,y]
+
 def banana(u, a = 1., b = .5, flag_noise = False):
 	rho = 0.95
 	Gamma = np.identity(2)
@@ -41,17 +74,6 @@ def banana(u, a = 1., b = .5, flag_noise = False):
 	x = u1 * a
 	y = u2/a - b *(u1**2 + a**2)
 	return np.array([x, y]) + flag_noise * np.linalg.cholesky(Gamma).dot(np.random.normal(0, 1, [2,]))
-
-def elliptic(u, x1 = 1./4, x2 = 3./4, dG = False, flag_noise = True, noise = 0.1):
-	u1, u2 = u
-	x = (u2 * x1) + (np.exp(-u1) * (-x1**2+x1) * 0.5) + (flag_noise * noise * np.random.normal())
-	y = (u2 * x2) + (np.exp(-u1) * (-x2**2+x2) * 0.5) + (flag_noise * noise * np.random.normal())
-
-	if dG:
-		DG = np.array([[-np.exp(-u1) * (-x1**2+x1) * 0.5, x1], [-np.exp(-u1) * (-x2**2+x2) * 0.5, x2]])
-		return DG
-	else:
-		return [x,y]
 
 class lorenz63(object):
 	"""
